641f2a4dd485e1dd10e10ca3ae608438ffa5565a
Inacio <67322986+Inaciocb@users.noreply.github.com>
2023-07-10 16:10:50 -0300

    atualizando API para retornar List

--- API REST/Dados.java
+++ API REST/Dados.java
@@ -1,28 +1,45 @@
 package inacio.api.demo.inacioapi;
+
 public class Dados {
     private int idade;
     private String nome;
     private String cargo;
+    private int ID;
 
-    public Dados(){  }
-    public Dados(int idade, String nome, String cargo) {
+    public Dados() {
+    }
+
+    public Dados(int idade, String nome, String cargo, int ID) {
         this.idade = idade;
         this.nome = nome;
         this.cargo = cargo;
+        this.ID = ID;
     }
 
-    public String getNome(){
+    public String getNome() {
         return this.nome;
     }
+
     public int getIdade() {
         return idade;
     }
+
     public void setIdade(int idade) {
         this.idade = idade;
     }
+
     public String getCargo() {
         return cargo;
     }
+
+    public int getID(){
+        return this.ID;
+    }
+
+    public void setID(int ID){
+        this.ID = ID;
+    }
+
     public void setCargo(String cargo) {
         this.cargo = cargo;
     }
--- API REST/DadosController.java
+++ API REST/DadosController.java
@@ -1,33 +1,47 @@
 package inacio.api.demo.inacioapi;
-
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Comparator;
+import java.util.Collections;
 
 @RestController
 @RequestMapping("/dados")
 public class DadosController {
 
-    private List<Dados> dados = new ArrayList<>();
-    assertTrue(dados.isEmpty());
-
+    private List<Dados> usuarios = new ArrayList<>();
     @GetMapping("/dados")
-    public Dados getDados() {
-        return dados;
+    public List<Dados> getUsuarios() {
+        usuarios.sort((u1, u2) -> Integer.compare(u1.getID(), u2.getID()));
+        return usuarios;
     }
 
     @PostMapping
-    public void setDados(@RequestBody Dados novoDados) {
-        dados = novoDados;
+    public List<Dados> setDados(@RequestBody Dados novoDados) {
+        usuarios.add(novoDados); // Add the new user to the list
+        usuarios.sort(Comparator.comparingInt(Dados::getID)); // Sort the list based on ID
+
+        for (int i = 0; i < usuarios.size(); i++) {
+            Dados usuario = usuarios.get(i);
+            usuario.setID(i + 1);
+        }
+
+        return usuarios;
     }
+
 
     @PutMapping
     public void updateDados(@RequestBody Dados atualizacaoDados) {
-        if (dados != null) {
-            dados.setIdade(atualizacaoDados.getIdade());
-            dados.setCargo(atualizacaoDados.getCargo());
+        for (Dados usuario : this.usuarios) {
+            if (usuario.getID() == atualizacaoDados.getID()) {
+                usuario.setIdade(atualizacaoDados.getIdade());
+                usuario.setCargo(atualizacaoDados.getCargo());
+                break;
+            }
         }
     }
     @DeleteMapping
     public void deleteDados() {
-        dados = null;
+        usuarios = null;
     }
 }
