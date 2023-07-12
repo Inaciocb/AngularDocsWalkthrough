2ca66f91bf37d4fabbcfce7b8a849de053e6c23b
Inacio <67322986+Inaciocb@users.noreply.github.com>
2023-07-12 16:00:14 -0300

    Botões

--- component/src/app/app-routing.module.ts
+++ component/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormularioComponent } from './components/formulario/formulario.component';
 import { BitcoinComponent } from './components/bitcoin/bitcoin.component';
 import { ListaUsuariosComponent} from './components/lista-usuarios/lista-usuarios.component';
+import { EditarUsuarioComponent} from './components/editar-usuario/editar-usuario.component';
 
 const routes: Routes = [
   { path: 'formulario', component: FormularioComponent },
   { path: 'bitcoin', component: BitcoinComponent },
-  {path: 'lista-usuarios', component: ListaUsuariosComponent}
+  {path: 'lista-usuarios', component: ListaUsuariosComponent},
+  {path: 'editar-usuario', component: EditarUsuarioComponent}
 ];
 
 @NgModule({
