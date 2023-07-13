f13e1b5e422926eb2d450f9a2999ab178234d27c
Inacio <67322986+Inaciocb@users.noreply.github.com>
2023-07-13 14:14:02 -0300

    pequenas ediçoes

--- component/src/app/components/editar-usuario/editar-usuario.component.ts
+++ component/src/app/components/editar-usuario/editar-usuario.component.ts
@@ -1,37 +1,43 @@
-  import { Component, OnInit } from '@angular/core';
-  import { ActivatedRoute } from '@angular/router';
-  import { UserListService, Dados } from 'src/app/services/user-list.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserListService, Dados } from 'src/app/services/user-list.service';
 
-  @Component({
-    selector: 'app-editar-usuario',
-    templateUrl: './editar-usuario.component.html',
-    styleUrls: ['./editar-usuario.component.css']
-  })
-  export class EditarUsuarioComponent implements OnInit {
-    userId: number = 0;
-    dados: Dados = new Dados();
-    dadosEnviados: Dados | null = null;
-    
-    constructor(private route: ActivatedRoute, private userListService: UserListService) { }
+@Component({
+  selector: 'app-editar-usuario',
+  templateUrl: './editar-usuario.component.html',
+  styleUrls: ['./editar-usuario.component.css']
+})
+export class EditarUsuarioComponent implements OnInit {
+  userId: number = 0;
+  dados: Dados = new Dados();
+  dadosEnviados: Dados | null = null;
+  
+  constructor(private route: ActivatedRoute, private userListService: UserListService) { }
 
-    
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.userId = +params['id'];
+      this.buscarUsuario();
+    });
+  }
 
-    ngOnInit(): void {
-      this.route.params.subscribe(params => {
-        this.userId = +params['id'];
-      });
-    }
+  buscarUsuario(): void {
+    this.userListService.buscarUsuarioPorId(this.userId).subscribe(response => {
+      this.dados = response;
+    });
+  }
 
-    editarDados(): void {
-      this.dados.id = this.userId;
-      this.userListService.atualizarUsuario(this.dados).subscribe(response => {
+  editarDados(): void {
+    this.dados.id = this.userId;
+    this.userListService.atualizarUsuario(this.dados).subscribe(response => {
       this.dadosEnviados = response;
     });
-}
-
-    removerUsuario(id: number): void {
-      this.userListService.removerUsuario(id).subscribe(response => {
-        this.dadosEnviados = response;
-      });
-    }
   }
+
+  removerUsuario(id: number): void {
+    this.userListService.removerUsuario(id).subscribe(response => {
+      this.dadosEnviados = response;
+    });
+  }
+}
+  
