cb2e1d6cacad088fbeda46d7b8e4bf3e919cbe07
Inacio <67322986+Inaciocb@users.noreply.github.com>
2023-07-27 16:03:44 -0300

    mudanças

---
+++ login/app/teste/page.tsx
@@ -0,0 +1,15 @@
+// Import statements for required modules or components
+import React from 'react';
+
+// Functional Component example
+const MyComponent: React.FC = () => {
+  // JSX elements and JavaScript/TypeScript code
+  return (
+    <div>
+      <h1 className="header">header</h1>
+      <p></p>
+    </div>
+  );
+};
+
+export default MyComponent;
